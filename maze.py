e864609a19d6035fae1bbcf423cc9dde49c7e1ff
MountiansUsual <albertwangyf@foxmail.com>
2019-12-18 15:00:52 +0800

    格式修改

--- maze.py
+++ maze.py
@@ -1,5 +1,12 @@
 import mazeworld
-simpleMaze=mazeworld.Maze([['#',' ',' ','S'],['~',' ',' ',' '],[' ',' ',' ','E']])
-print(simpleMaze)
-simpleMazeAgent=mazeworld.DepthFirstSearchAgent()
-mazeworld.testAgentOnMaze(simpleMazeAgent,simpleMaze)
+
+if __name__ == "__main__":
+    simpleMaze = mazeworld.Maze(
+    [['#', ' ', ' ', 'S'], ['~', ' ', ' ', ' '], [' ', ' ', ' ', 'E']])
+    
+    print(simpleMaze)
+    simpleMazeAgent = mazeworld.DepthFirstSearchAgent()
+
+    mazeworld.testAgentOnMaze(simpleMazeAgent, simpleMaze)
+
+
